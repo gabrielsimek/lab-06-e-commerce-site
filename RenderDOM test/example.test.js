b296b14599e7b2922c230b36a1f931224021d28d
Gabriel Simek <gabrielsimek@outlook.com>
2021-03-31 19:56:58 -0700

    refactor gettotalprice to two seperate function

--- RenderDOM test/example.test.js
+++ RenderDOM test/example.test.js
@@ -13,91 +13,20 @@ test('create and return table row with table data appended to it based on inform
     //Act 
     // Call the function you're testing and set the result to a const
     const actual = createCartTd(
-        [
-            {
-                id: 1,
-                quantity: 4
-            },
-            {
-                id: 4,
-                quantity: 2
-            },
-            {
-                id: 2,
-                quantity: 4
-            },
-        ],
-        [
-            {
-                id: 1,
-                type: 'fresh-fruit',
-                name: 'Red Apple',
-                color: 'red',
-                image: `../assets/apple.jpg`,
-                isCruncy: true,
-                isOrganic: false,
-                price: 3
-            },
-            {
-                id: 2,
-                type: 'fresh-fruit',
-                name: 'Banana',
-                color: 'yellow',
-                image: `../assets/banana.jpg`,
-                isCruncy: false,
-                isOrganic: true,
-                price: 2
-            },
-            {
-                id: 3,
-                type: 'frozen-fruit',
-                name: 'Blueberries',
-                color: 'blue',
-                image: '../assets/blueberries.jpg',
-                isCruncy: false,
-                isOrganic: false,
-                price: 5
-            },
-            {
-                id: 4,
-                type: 'fresh-fruit',
-                name: 'Cantaloupe',
-                color: 'green',
-                image: '../assets/cantaloupe.jpg',
-                isCruncy: false,
-                isOrganic: false,
-                price: 4
-            },
-            {
-                id: 5,
-                type: 'frozen-fruit',
-                name: 'Strawberries',
-                color: 'red',
-                image: '../assets/strawberries.jpg',
-                isCruncy: false,
-                isOrganic: true,
-                price: 6
-            },
-            
-            { 
-                id: 6,
-                type: 'fresh fruit',
-                name: 'Manderins',
-                color: 'orange',
-                image: '../assets/mandarins.jpg',
-                isCruncy: false,
-                isOrganic: false,
-                price: 2
-            },
-            
-            
-            
-        
-
-        ]
-        
-
- 
+        {
+            id: 1,
+            quantity: 4
+        },
+        {
+            id: 1,
+            type: 'fresh-fruit',
+            name: 'Red Apple',
+            color: 'red',
+            image: `../assets/apple.jpg`,
+            isCruncy: true,
+            isOrganic: false,
+            price: 3
+        }
      
     );
 
--- calcItemTotaltest/example.test.js
+++ calcItemTotaltest/example.test.js
@@ -1,19 +1,104 @@
 // IMPORT MODULES under test here:
 // import { example } from '../example.js';
-import { cart } from '../cart/cart-data.js';
+
 import { calcItemTotal } from '../utils.js';
 const test = QUnit.test;
-import { fruits } from '../products/products.js';
+
 test('derive the total price for the cart my multiply the quantity of each item by its price and adding all totals togeter. should take in the fruits array and the cart array', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = (4 * 3) + (2 * 4) + (2 * 4);
+    const expected = 48;
     
    
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = calcItemTotal(cart, fruits);
+    const actual = calcItemTotal([
+        {
+            id: 1,
+            quantity: 4
+        },
+        {
+            id: 4,
+            quantity: 2
+        },
+        {
+            id: 2,
+            quantity: 4
+        },
+        {
+            id: 3,
+            quantity: 4
+        },
+        
+    ], [
+        {
+            id: 1,
+            type: 'fresh-fruit',
+            name: 'Red Apple',
+            color: 'red',
+            image: `../assets/apple.jpg`,
+            isCruncy: true,
+            isOrganic: false,
+            price: 3
+        },
+        {
+            id: 2,
+            type: 'fresh-fruit',
+            name: 'Banana',
+            color: 'yellow',
+            image: `../assets/banana.jpg`,
+            isCruncy: false,
+            isOrganic: true,
+            price: 2
+        },
+        {
+            id: 3,
+            type: 'frozen-fruit',
+            name: 'Blueberries',
+            color: 'blue',
+            image: '../assets/blueberries.jpg',
+            isCruncy: false,
+            isOrganic: false,
+            price: 5
+        },
+        {
+            id: 4,
+            type: 'fresh-fruit',
+            name: 'Cantaloupe',
+            color: 'green',
+            image: '../assets/cantaloupe.jpg',
+            isCruncy: false,
+            isOrganic: false,
+            price: 4
+        },
+        {
+            id: 5,
+            type: 'frozen-fruit',
+            name: 'Strawberries',
+            color: 'red',
+            image: '../assets/strawberries.jpg',
+            isCruncy: false,
+            isOrganic: true,
+            price: 6
+        },
+        
+        { 
+            id: 6,
+            type: 'fresh fruit',
+            name: 'Manderins',
+            color: 'orange',
+            image: '../assets/mandarins.jpg',
+            isCruncy: false,
+            isOrganic: false,
+            price: 2
+        },
+        
+        
+        
+    
+    
+    ]);
     
 
     //Expect
